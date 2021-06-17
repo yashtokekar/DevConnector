7916f9f7f0c56eee37faaa7b2ceeafb6f081ed56
Yash Tokekar <yash.tokekar@gmail.com>
2021-06-17 12:18:07 +0530

    Creating User Model

--- server.js
+++ server.js
@@ -6,6 +6,15 @@ const app = express();
 //Connect Database
 connectDB();
 
+
+//Define Routes
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/profile', require('./routes/api/profile'));
+app.use('/api/posts', require('./routes/api/posts'));
+
+
+
 app.get('/', (req,res) => {res.send('API Running')} );
 
 const PORT = process.env.PORT || 5000
