9d2d9420de3b639e5ae5aa88f149c37d4253e4c5
Yash Tokekar <yash.tokekar@gmail.com>
2021-08-01 09:14:17 +0530

    Add and Delete Comment

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -124,6 +124,7 @@ router.get('/user/:user_id', async (req,res) => {
         const profile = await Profile.findOne({ user: req.params.user_id }).populate('user', ['name','avatar']);
         
         if(!profile) return res.status(400).json({ msg: 'Profile not found'});
+        
         res.json(profile);
     } catch (err) {
         console.error(err.message);
