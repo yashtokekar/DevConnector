f97f2e5ca7dfe8e5943807ad402876e9f3f87053
Yash Tokekar <yash.tokekar@gmail.com>
2021-08-10 12:46:47 +0530

    Starting on profile

--- client/src/components/profile-forms/AddEducation.js
+++ client/src/components/profile-forms/AddEducation.js
@@ -79,4 +79,4 @@ AddEducation.propTypes = {
     addEducation: PropTypes.func.isRequired,
 }
 
-export default connect(null, { addEducation })(AddEducation)
+export default connect(null, { addEducation })(withRouter(AddEducation));
