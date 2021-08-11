4b109f48b8b58b9a3bb586687ab02b934ae8b1e4
Yash Tokekar <yash.tokekar@gmail.com>
2021-08-11 09:06:38 +0530

    Post Reducer, Action & Initial Component

--- client/src/App.js
+++ client/src/App.js
@@ -12,6 +12,7 @@ import AddExperience from './components/profile-forms/AddExperience';
 import AddEducation from './components/profile-forms/AddEducation';
 import Profiles from './components/profiles/Profiles';
 import Profile from './components/profile/Profile';
+import Posts from './components/posts/Posts';
 import PrivateRoute from './components/routing/PrivateRoute';
 //Redux
 import { Provider } from 'react-redux';
@@ -20,6 +21,7 @@ import { loadUser } from './actions/auth';
 import setAuthToken from './utils/setAuthToken';
 
 import './App.css';
+import { getPosts } from './actions/post';
 
 if(localStorage.token) {
   setAuthToken(localStorage.token);
@@ -48,6 +50,7 @@ const App = () => {
           <PrivateRoute exact path= '/edit-profile' component = {EditProfile} />
           <PrivateRoute exact path= '/add-experience' component = {AddExperience} />
           <PrivateRoute exact path= '/add-education' component = {AddEducation} />
+          <PrivateRoute exact path= '/posts' component = {Posts} />
         </Switch>
       </section>
     </Fragment>
